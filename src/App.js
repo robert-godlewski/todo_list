79e9b09fcdcf84cdad01a2fe24327193c1b08703
Robert Godlewski <robtechg5@gmail.com>
2022-03-23 20:22:59 -0700

    Added form.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import React, {setState} from 'react';
+import ItemForm from './components/ItemForm';
 import './App.css';
 
 function App() {
+  const [todoList, setTodoList] = setState([])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ItemForm todoList={todoList} setTodoList={setTodoList} />
     </div>
   );
 }
---
+++ src/components/ItemForm.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+
+const ItemForm = (props) => {
+    const {todoList, setTodoList} = props
+    const [details, setDetails] = useState("");
+
+    // const addColor = (e) => {
+    //     e.preventDefault();
+    //     setBoxColorList([...boxColorList, color]);
+    //     setColor("");
+    // };
+
+    const addItem = (e) => {
+        e.preventDefault();
+        setTodoList([
+            ...todoList, 
+            {
+                details: details, 
+                is_finished: false
+            }
+        ]);
+        setDetails("");
+    };
+
+    return (
+        <div>
+            <form onSubmit={addItem}>
+                <div>
+                    <label>Details </label>
+                    <input type="text" name="details" value={details} onChange={(e) => setDetails(e.target.value)}/>
+                </div>
+                <button>Add</button>
+            </form>
+        </div>
+    );
+}
+
+export default ItemForm;
